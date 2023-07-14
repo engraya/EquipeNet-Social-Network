75a6fbebd65e1c724379a33c57249bbc80e5cecb
Engraya <ahmadyakubuaya@gmail.com>
2023-07-14 20:17:19 +0100

    Commit Changes

--- network/models.py
+++ network/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django_countries.fields import CountryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    GENDER_CHOICES = (
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    userName = models.CharField(max_length=100)
+    userID = models.IntegerField()
+    phoneNumber = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    profilePicture = models.ImageField(upload_to='profileImages', default='')
+    dateOfBirth = models.DateField(auto_now=False, auto_now_add=False)
+    age = models.IntegerField()
+    profession = models.CharField(max_length=100)
+    country = CountryField()
+    location = models.CharField(max_length=200)
+    about = models.TextField(max_length=200)
+    facebook = models.URLField(max_length=200)
+    twitter = models.URLField(max_length=200)
+    instagram = models.URLField(max_length=200)
+    linkEdin = models.URLField(max_length=200)
+    gitHub = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
--- network/views.py
+++ network/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Social Media Page")
+
+
+
+def homeFeeds(request):
+    pass
+
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "User Registration Succesful...")
+            return redirect("homeFeeds")
+        else:
+            messages.error(request, "Registration Unsuccessful, Invalid Credentials Entered!!")
+
+    else:
+        form = UserRegistrationForm()
+
+    context = {}
+    return render(request, 'network/registerUser.html', context)
+
+
+def signIn(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"Congratulations, you are now logged in as {username}.")
+                return redirect("homeFeeds")
+            else:
+                messages.error(request, "Invalid Credentials Entered..Try again Later!!")
+
+        else:
+            messages.error(request, "Invalid Username or Password Used....Try again Later!!!")
+    else:
+        form = UserLoginForm()
+    context = {}
+    return render(request, 'network/loginUser.html', context)
+
+        
+def signOut(request):
+    logout(request)
+    messages.info(request, f"{username} Successfully Logged out!!")
+    return redirect("loginPage")
+
+
+
