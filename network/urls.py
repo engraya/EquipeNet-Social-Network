20bf8eb0ff39209e82a61d249c851446a2f86aed
Engraya <ahmadyakubuaya@gmail.com>
2023-07-17 15:14:23 +0100

    Password Reset Commit

--- network/urls.py
+++ network/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,13 @@ urlpatterns = [
     path('signUp', views.signUp, name='register'),
     path('signIn', views.signIn, name='login'),
     path('signOut', views.signOut, name='logout'),
+
+
+
+
+    #password Reset urls
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="network/passwordManage/password_reset_form.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="network/passwordManage/password_reset_sent.html"), name="password_reset_sent"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="network/passwordManage/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="network/passwordManage/password_reset_complete.html"), name="password_reset_complete")
 ]
